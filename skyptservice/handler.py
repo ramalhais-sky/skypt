52bf4245d2dbae8ff6a8385aa5104dab008d6b60
Almeida <pal31@devml0244672.bskyb.com>
2020-01-24 19:52:27 +0000

    secrets

--- skyptservice/handler.py
+++ skyptservice/handler.py
@@ -1,32 +1,34 @@
 import json
 import ldap
+import os
 
-con = ldap.initialize('ldap://xxx.xxx.com')
-con.simple_bind_s("philip.xxx@xxx","xxx")
-ldap_base = "OU=xxx,OU=xxs,dc=xxx,dc=com"
+env_ldapnode = os.environ['skypt_ldapnode']
+env_ldapuser = os.environ['skypt_ldapuser']
+env_ldappass = os.environ['skypt_ldappass']
+env_ldapbase = os.environ['skypt_ldapbase']
+
+con = ldap.initialize("ldap://"+env_ldapnode)
+con.simple_bind_s(env_ldapuser,env_ldappass)
+ldap_base = env_ldapbase
+
+def getValue(data,key):
+    if key in data.keys():
+        return data[key][0]
+    else:
+        return ""
 
 def getuser(event, context):
 
-    query = f"(& (objectclass=user) (cn=*{event['data']['username']}*))"
+    query = f"(& (objectclass=user) (cn=*{event['data']['username'].replace(' ','*')}*))"
     result = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query)
     results = []
-    for dn, entry in result:
-        try:
-            m = entry['mobile'][0]
-        except:
-            x = {
-                "cn": entry['cn'][0],
-                "user": entry['sAMAccountName'][0],
-                "email": entry['mail'][0],
-                "mobile": ""
-            }
-        else:
-            x = {
-                "cn": entry['cn'][0],
-                "user": entry['sAMAccountName'][0],
-                "email": entry['mail'][0],
-                "mobile": entry['mobile'][0]
-            }
+    for dn, user in result:
+        x = {
+            "cn": getValue(user,"cn"),
+            "user": getValue(user,"sAMAccountName"),
+            "email": getValue(user,"mail"),
+            "mobile": getValue(user,"mobile")
+        }
         results.append(x)
 
     body = {
@@ -41,4 +43,9 @@ def getuser(event, context):
 
     return response
 
-#print(json.dumps(getuser({"data":{"username":'ramalhais'}}, '')))
+# Local test set environment vars
+# export skypt_ldapnode=
+# export skypt_ldapuser=
+# export skypt_ldappass=''
+# export skypt_ldapbase=''
+# print(json.dumps(getuser({"data":{"username":'dave'}}, '')))
